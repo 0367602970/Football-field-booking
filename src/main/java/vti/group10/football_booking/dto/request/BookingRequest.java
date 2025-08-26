a0525a3d5e775243ca1b9f3e7928a2716941dac6
hocnghiemtuc <dominhduc258@gmail.com>
2025-08-26 13:56:27 +0700

    booking

---
+++ src/main/java/vti/group10/football_booking/dto/request/BookingRequest.java
@@ -0,0 +1,14 @@
+package vti.group10.football_booking.dto.request;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+public class BookingRequest {
+    private Long fieldId;
+    private LocalDate bookingDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
+}
--- src/main/java/vti/group10/football_booking/repository/BookingRepository.java
+++ src/main/java/vti/group10/football_booking/repository/BookingRepository.java
@@ -2,10 +2,19 @@ package vti.group10.football_booking.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import vti.group10.football_booking.model.Booking;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-    
+    List<Booking> findByField_IdAndBookingDateAndStatusAndStartTimeLessThanAndEndTimeGreaterThan(
+            Integer fieldId,
+            LocalDate bookingDate,
+            Booking.Status status,
+            LocalTime endTime,
+            LocalTime startTime
+    );
 }
