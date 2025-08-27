a4a8ca2a3c67544ee18ca500cd51445e8356a5af
0367602970 <toan0230667@huce.edu.vn>
2025-08-27 23:52:11 +0700

    add image file

---
+++ src/main/java/vti/group10/football_booking/dto/response/FieldImageResponse.java
@@ -0,0 +1,13 @@
+package vti.group10.football_booking.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FieldImageResponse {
+    private int id;
+    private String imageUrl;
+}
